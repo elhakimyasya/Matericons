8f755bb306d0e04a91d028831e5288f677722074
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-12-20 02:20:10 +0700

    Create icons.js

---
+++ src/icons.js
@@ -0,0 +1,8 @@
+import Icon from './icon';
+import icons from '../dist/icons.json';
+import tags from './icon-tags.json';
+
+export default Object.keys(icons).map(key => new Icon(key, icons[key], tags[key])).reduce((object, icon) => {
+    object[icon.name] = icon;
+    return object;
+}, {});
