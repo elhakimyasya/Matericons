1390d7c90f9b800b80fd82f702e579a42f6f548e
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-12-20 02:11:54 +0700

    Create icon-build-svg-sprite-string.js

---
+++ bin/icon-build-svg-sprite-string.js
@@ -0,0 +1,15 @@
+import DEFAULT_ATTRS from '../src/icon-attributes.json';
+
+// Build an SVG sprite string containing SVG symbols
+function iconBuildSVGSpriteString(icons) {
+    const symbols = Object.keys(icons).map(icon => toSvgSymbol(icon, icons[icon])).join('');
+
+    return `<svg xmlns="${DEFAULT_ATTRS.xmlns}" width="24" height="24"><defs>${symbols}</defs></svg>`;
+}
+
+// Create an SVG symbol string
+function toSvgSymbol(name, contents) {
+    return `<symbol id="${name}" viewBox="${DEFAULT_ATTRS.viewBox}">${contents}</symbol>`;
+}
+
+export default iconBuildSVGSpriteString;
